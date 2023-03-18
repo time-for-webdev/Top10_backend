ea213a94b91f580065898d789ca65891f105c86b
piyushcse44 <piyushkumarcse44@gmail.com>
2023-03-18 23:51:51 +0530

    added all section of vpn

--- Top_10_Vpn/vpn/models.py
+++ Top_10_Vpn/vpn/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 import uuid
 from django.core.validators import MaxValueValidator
+from django.core.exceptions import ValidationError
+
+
+
 # Create your models here.
 
 class VpnList(models.Model):
@@ -18,7 +22,7 @@ class VpnList(models.Model):
         
 class All_avilable_filter(models.Model):
     id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200,unique=True)
 
     def __str__(self):
         return self.name
@@ -37,25 +41,41 @@ class Top_ten(models.Model):
     Ninth = models.ForeignKey(VpnList,on_delete=models.PROTECT,null=True,blank=True,related_name='top_ten_ninth_set')    
     Tenth = models.ForeignKey(VpnList,on_delete=models.PROTECT,null=True,blank=True,related_name='top_ten_tenth_set')    
 
+
+    def clean(self):
+        # Check for duplicates in the First to Tenth fields
+        first_to_tenth = [self.First, self.Second, self.Third, self.Forth, self.Fifth, self.Sixth, self.Seventh, self.Eighth, self.Ninth, self.Tenth]
+        non_empty_fields = [field for field in first_to_tenth if field is not None]
+        if len(non_empty_fields) != len(set(non_empty_fields)):
+            raise ValidationError('The First to Tenth fields must be unique.')
+
     def __str__(self):
         return str(self.id)
 
 class Device(models.Model):
     id = models.IntegerField(unique=True,primary_key=True,editable=False)
-    name = models.ForeignKey(All_avilable_filter,on_delete=models.CASCADE,)
+    name = models.OneToOneField(All_avilable_filter,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name.name
+
+    
+    
 
     
 
 class Location(models.Model):
     id = models.IntegerField(unique=True,primary_key=True,editable=False)
-    name = models.ForeignKey(All_avilable_filter,on_delete=models.CASCADE)
+    name = models.OneToOneField(All_avilable_filter,on_delete=models.CASCADE)
 
-   
+    def __str__(self):
+        return self.name.name
 
 class Service(models.Model):
     id = models.IntegerField(unique=True,primary_key=True,editable=False)
-    name = models.ForeignKey(All_avilable_filter,on_delete=models.CASCADE)
+    name = models.OneToOneField(All_avilable_filter,on_delete=models.CASCADE)
 
-                 
-            
+    def __str__(self):
+        return self.name.name
+    
 
