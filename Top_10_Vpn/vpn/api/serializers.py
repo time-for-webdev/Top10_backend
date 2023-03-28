02ba1278838f4ccd29e2491829405a8d54cefe94
piyushcse44 <piyushkumarcse44@gmail.com>
2023-03-29 02:43:54 +0530

    update location

--- Top_10_Vpn/vpn/api/serializers.py
+++ Top_10_Vpn/vpn/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from vpn.models import Top_ten,VpnList,All_avilable_filter,Form,Specification,remark,Comparision
+from vpn.models import VpnList,Form,Specification,remark,Comparision,Device,Location,Service
 
 class Comparision_Serializer(serializers.ModelSerializer):
     class Meta:
@@ -30,29 +30,48 @@ class VpnList_Serializer(serializers.ModelSerializer):
         fields = '__all__'    
 
 
-class All_available_filter_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = All_avilable_filter
-        fields = '__all__'   
-
 class Form_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Form
-        fields = '__all__'         
+        fields = '__all__'       
 
-class Top_Ten_Serializer(serializers.ModelSerializer):
-    type =  All_available_filter_Serializer()
+class Device_Serializer(serializers.ModelSerializer):
     First = VpnList_Serializer()  
     Second = VpnList_Serializer()   
     Third = VpnList_Serializer()  
     Forth = VpnList_Serializer()  
     Fifth = VpnList_Serializer()  
     Sixth = VpnList_Serializer()  
-    Seventh = VpnList_Serializer()    
-    Eighth = VpnList_Serializer()  
-    Ninth = VpnList_Serializer()  
-    Tenth = VpnList_Serializer()
-    
+    Seventh = VpnList_Serializer()   
+
     class Meta:
-        model = Top_ten
-        fields = '__all__'
+        model = Device
+        fields = '__all__'      
+
+class Location_Serializer(serializers.ModelSerializer):
+    First = VpnList_Serializer()  
+    Second = VpnList_Serializer()   
+    Third = VpnList_Serializer()  
+    Forth = VpnList_Serializer()  
+    Fifth = VpnList_Serializer()  
+    Sixth = VpnList_Serializer()  
+    Seventh = VpnList_Serializer()   
+
+    class Meta:
+        model = Location
+        fields = '__all__'    
+
+class Service_Serializer(serializers.ModelSerializer):
+    First = VpnList_Serializer()  
+    Second = VpnList_Serializer()   
+    Third = VpnList_Serializer()  
+    Forth = VpnList_Serializer()  
+    Fifth = VpnList_Serializer()  
+    Sixth = VpnList_Serializer()  
+    Seventh = VpnList_Serializer()   
+
+    class Meta:
+        model = Service
+        fields = '__all__'                  
+
+
