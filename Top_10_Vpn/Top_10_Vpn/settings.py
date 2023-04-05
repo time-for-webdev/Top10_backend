1d093c40260808d585e8fd792fd43244147d40e4
piyushcse44 <piyushkumarcse44@gmail.com>
2023-04-05 23:07:26 +0530

    fix typo bug

--- Top_10_Vpn/Top_10_Vpn/settings.py
+++ Top_10_Vpn/Top_10_Vpn/settings.py
@@ -130,9 +130,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
-STATICFILE_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'media/')
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
